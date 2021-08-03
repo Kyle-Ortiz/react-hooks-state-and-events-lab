d4b5bc72b485256ce225ff227017f75fd56552d5
Kyle Ortiz <kyleaortiz@gmail.com>
2021-08-03 16:17:40 -0500

    commit tree f77c6acfc6f1e364e675f48a5459a43de6948cbc

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,20 @@ function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [theme,setTheme]= useState(false);
+  const appClass = theme ? "App dark" : "App light"
+
+
+  function themeHandler() {
+   setTheme(!theme);
+  }
+
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={themeHandler}>{theme ? "Dark" : "Light"} Mode</button>
       </header>
       <ShoppingList items={items} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [item,setItem] = useState("false")
+  const itemClass = item ? "" : "in-cart";
+
+  function addHandler() {
+    setItem(!item)
+  }
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addHandler}>Add to Cart</button>
     </li>
   );
 }
